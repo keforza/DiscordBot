19f4224a3b884e1e2dde41ea03123f03db780cb3
K3Forza <formentinkosta843@gmail.com>
2025-06-27 14:00:19 +0200

    Aggiunta thumbnail alla risposta del bot

--- interactions/minecraftIpSelect.js
+++ interactions/minecraftIpSelect.js
@@ -1,6 +1,5 @@
 // interactions/minecraftIpSelect.js
 const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, MessageFlags } = require('discord.js');
-// ^^^ IMPORTANTE: Ho aggiunto MessageFlags qui!
 
 module.exports = {
     customId: 'minecraft_edition_select', 
@@ -15,20 +14,24 @@ module.exports = {
 
         let title = '';
         let description = '';
+        let thumbnailUrl = ''; // Nuova variabile per l'URL della thumbnail
         const color = '#2ECC71'; 
         
         switch (selectedEdition) {
             case 'java':
                 title = 'Minecraft Java Edition IP';
                 description = `Connettiti al server Java usando questo indirizzo:\n\n**\`${serverIP.toUpperCase()}:${serverPort}\`**`;
+                thumbnailUrl = 'https://cdn.discordapp.com/attachments/1338470846154543134/1388125898997497896/minecraft_java.png?ex=685fd87c&is=685e86fc&hm=dec8bafe9ee9a72c2f53afc00976d234f35d26078a7f5fb919f4de5cc0c04e99&'; // Thumbnail per Java
                 break;
             case 'bedrock':
                 title = 'Minecraft Bedrock Edition IP & Porta';
                 description = `Connettiti al server Bedrock usando questo indirizzo e porta:\n\nIndirizzo: **\`${serverIP.toUpperCase()}\`**\nPorta: **\`${serverPort}\`**`;
+                thumbnailUrl = 'https://cdn.discordapp.com/attachments/1338470846154543134/1388125898686857349/minecraft_bedrock.png?ex=685fd87c&is=685e86fc&hm=e589e6b39a646cc796c49b31c2c82bf398476a70ed0cc0b8aa5d0ca830a25e20&'; // Thumbnail per Bedrock
                 break;
             default:
                 title = 'Errore';
                 description = 'Selezione non valida.';
+                thumbnailUrl = ''; // Nessuna thumbnail per errore
                 break;
         }
 
@@ -36,13 +39,18 @@ module.exports = {
             .setColor(color)
             .setTitle(title)
             .setDescription(description)
-            .setThumbnail('https://cdn.discordapp.com/attachments/1338470846154543134/1388071543669522462/server_Minecraft.png?ex=685fa5dd&is=685e545d&hm=9d81694716f1dde9387e60579df30ad97bc09157a1149cd4c1f8cb31bfa0204f&');
+            .setImage('https://cdn.discordapp.com/attachments/1338470846154543134/1388071543669522462/server_Minecraft.png?ex=685fa5dd&is=685e545d&hm=9d81694716f1dde9387e60579df30ad97bc09157a1149cd4c1f8cb31bfa0204f&');
+            
+        // Aggiungi la thumbnail solo se l'URL è definito
+        if (thumbnailUrl) {
+            embed.setThumbnail(thumbnailUrl);
+        }
 
         // La logica di editReply rimane la stessa, dato che l'ephemeral è già gestito da deferReply con le flags
         await interaction.editReply({ 
             content: 'Ecco le informazioni richieste:', 
             embeds: [embed], 
-            components: [], 
+            components: [], // Rimuovi i componenti per pulire il messaggio di risposta
         });
 
         // --- Logica per resettare il dropdown nel messaggio ORIGINALE ---
