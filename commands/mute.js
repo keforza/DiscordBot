076c5b18257f0b9c647e62977dced811bebb18d1
K3Forza <formentinkosta843@gmail.com>
2025-08-21 16:09:15 +0200

    Improved timeout of users

--- commands/mute.js
+++ commands/mute.js
@@ -88,8 +88,8 @@ module.exports = {
                             .setColor('#00FF00') // Green color for unmuted
                             .setTitle('You got unmuted')
                             .addFields(
-                                { name: 'Reason', inline: true,  value: 'Expired', inline: true },
-                                { name: 'Responsible', inline: true, value: `${interaction.client.user.tag}`, inline: true }
+                                { name: 'Reason', value: 'Expired', inline: true },
+                                { name: 'Responsible', value: `${interaction.client.user.tag} (BOT)`, inline: true }
                             )
                             .setTimestamp();
                         
